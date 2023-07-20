6c62a511943c406c346255700b11c9ee991162eb
shoji_hajime <shoji_hajime@giginc.co.jp>
2023-07-20 17:15:01 +0900

    fix: design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://hajimemath.com',
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
